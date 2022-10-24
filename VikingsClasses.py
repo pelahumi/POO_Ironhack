f37c65da4920035ea2a0bef79d40ab2bfebdb172
Pelayo <phuermij@myuax.com>
2022-10-24 10:34:57 +0200

    class soldier

---
+++ VikingsClasses.py
@@ -0,0 +1,12 @@
+class Soldier():
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        
+
