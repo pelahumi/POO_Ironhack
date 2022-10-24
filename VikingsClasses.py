7e5fbbdf407bfe68e9ac2257e7f9a8ec7b14fc2f
Pelayo <phuermij@myuax.com>
2022-10-24 11:27:47 +0200

    class war

--- VikingsClasses.py
+++ VikingsClasses.py
@@ -1,3 +1,5 @@
+import random
+
 class Soldier():
     def __init__(self, health, strength):
         self.health = health
@@ -30,9 +32,6 @@ class Saxon(Soldier):
     def __init__(self, health, strength):
         super().__init__(health, strength)
     
-    def attack(self):
-        return super().attack()
-    
     def receiveDamage(self, damage):
         self.health = self.health - damage
 
@@ -41,3 +40,48 @@ class Saxon(Soldier):
         
         else:
             return "A Saxon has died in combat"
+
+class War():
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def addViking(self, viking):
+        self.vikingArmy.append(viking)
+        return None
+    
+    def addSaxon(self, saxon):
+        self.saxonArmy.append(saxon)
+        return None
+        
+    def vikingAttack(self):
+        saxon = Saxon(random.choice(self.saxonArmy))
+        viking = Viking(random.choice(self.vikingArmy))
+        saxon.receiveDamage(viking.strength)
+        if saxon.health == 0:
+            self.saxonArmy.remove(saxon)
+        else:
+            return "A Saxon has received {} of damage".format(viking.strength)
+        
+    def saxonAttack(self):
+        viking = Viking(random.choice(self.vikingArmy))
+        saxon = Saxon(random.choice(self.saxonArmy))
+        viking.receiveDamage(saxon.strength)
+        if viking.health == 0:
+            self.vikingArmy.remove(viking)
+        else:
+            return "{} has received {} of damage".format(viking.name, saxon.strength)
+
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day.."
+        elif len(self.saxonArmy) == 1 and len(self.vikingArmy) == 1:
+            return "Vikings and Saxons are still in the thick of battle"
+        
+
+        
+
+
+        
